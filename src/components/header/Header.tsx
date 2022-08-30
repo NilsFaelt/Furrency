1a2e127275346d0264363401da0e895effd80881
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-08-30 10:33:14 +0200

    resposnive meny

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,9 @@
 import Styles from "./header.module.css";
-import { Button } from "@mui/material";
 import { MenuIcon } from "@heroicons/react/outline";
 import { useState } from "react";
 import { ShoppingCartIcon } from "@heroicons/react/solid";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 interface Currencys {
   toPay: number;
   symbol: String;
@@ -52,6 +52,14 @@ const Header: React.FC<Props> = ({
         onClick={() => setToogleMenu(!toogleMenu)}
         className={Styles.burger}
       />
+      <nav className={Styles.nav}>
+        <Link className={Styles.link} to={"/"}>
+          Currency Exchange
+        </Link>
+        <Link className={Styles.link} to={"/kryptoexchange"}>
+          Crypto Exchange
+        </Link>
+      </nav>
       <div className={Styles.copuntryDiv}>
         <input
           onChange={(e) => setChooseFlag(e.target.value)}
